496ca7be997ae7b35f95ce1e56fea246f676b07c
Chinen Suguru <suguru.chinen@gmail.com>
2026-06-12 09:37:48 +0000

    Update mdviewer to v0.6.31

--- Casks/mdviewer.rb
+++ Casks/mdviewer.rb
@@ -1,6 +1,6 @@
 cask "mdviewer" do
-  version "0.6.30"
-  sha256 "4ee4351e7228bfd531c704b153ffa2f7960e56c33753307204dd2128514c9637"
+  version "0.6.31"
+  sha256 "12d094ccd96a9723716f59545c0a48fde94c3e648d9e3b2f85a6aa46da2a3301"
   
   url "https://github.com/buckmoon/mdviewer-release/releases/download/v#{version}/mdViewer-#{version}-arm64.dmg"
   name "MDViewer"
