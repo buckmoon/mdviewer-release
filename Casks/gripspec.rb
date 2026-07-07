cecb44c7bb8fe79d39ccd7fd96a6ad16050e7d83
Chinen Suguru <suguru.chinen@gmail.com>
2026-07-07 19:02:55 +0000

    Update gripspec to v0.6.61

---
+++ Casks/gripspec.rb
@@ -0,0 +1,17 @@
+cask "gripspec" do
+  version "0.6.61"
+  sha256 "894ea39b16ed91a198eac735c8e243cefd36f23aedeb12ffe4b5442510cf0104"
+  
+  url "https://github.com/buckmoon/gripspec-release/releases/download/v#{version}/GripSpec-#{version}-arm64.dmg"
+  name "GripSpec"
+  desc "Fast and lightweight Markdown viewer with live preview"
+  homepage "https://github.com/buckmoon/gripspec"
+  
+  app "GripSpec.app"
+  
+  zap trash: [
+    "~/Library/Application Support/gripspec",
+    "~/Library/Preferences/jp.co.buckmoon.mdviewer.plist",
+    "~/Library/Caches/gripspec"
+  ]
+end
