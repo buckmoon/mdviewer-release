206b4030df2d78de37338c5084f394287e996ac7
Chinen Suguru <suguru.chinen@gmail.com>
2026-07-10 13:42:54 +0000

    Update gripspec to v0.6.68

--- Casks/gripspec.rb
+++ Casks/gripspec.rb
@@ -1,6 +1,6 @@
 cask "gripspec" do
-  version "0.6.67"
-  sha256 "0416c0f0ca1aa1c94a10cbb45543b98a413d475a93b027fef7e112b0c8566c91"
+  version "0.6.68"
+  sha256 "66c1ac4a76e80db340b1155bc88d39fa30d74e9e0991a091763cb1eaa05a2572"
   
   url "https://github.com/buckmoon/gripspec-release/releases/download/v#{version}/GripSpec-#{version}-arm64.dmg"
   name "GripSpec"
